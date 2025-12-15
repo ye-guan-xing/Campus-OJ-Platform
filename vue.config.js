4970543e92a4591a0a6329faa9e0b5f95e768828
ye-guan-xing <2024352805@qq.com>
2025-12-15 21:10:53 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,6 @@ module.exports = {
       [VUE_APP_BASE_API]: {
         target: VUE_APP_PROXY_TARGET,
         changeOrigin: true,
-        // 因为我们的后端接口路径有/api，所以不需要重写，直接转发过去
         // 如果后端接口没有/api，那么需要重写去掉/api
         // 这里后端有/api，所以不重写，即保留/api
         pathRewrite: {
